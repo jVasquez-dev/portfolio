8cb6578ff36265e2b059c0e49e6b611e55de03d1
Jorge Vasquez <jvasquezn04@gmail.com>
2022-01-19 22:41:43 -0600

    gift app added

--- src/helpers/projectsText.js
+++ src/helpers/projectsText.js
@@ -2,12 +2,19 @@ export const projects = [{
     id: 1,
     title: 'To Do App',
     img: 'https://res.cloudinary.com/dob4dwvni/image/upload/v1635983794/to-do-app_nzehhc.png',
-    description: `An app to keep track of to do tasks. Made with React, Bootstrap and using local storage`,
+    description: `An app to keep track of your tast to do. Made with React, Bootstrap and using local storage`,
     link: 'https://to-do-list-app-j.netlify.app/'
 }, {
     id: 2,
     title: 'Journal App',
     img: 'https://res.cloudinary.com/dob4dwvni/image/upload/v1635983965/journal-app_ibz5vr.png',
-    description: 'A journal to keep track of your activities. I made it using React, Redux, SASS.',
+    description: 'A journal to save your thougths and activities . I made it using React, Redux, SASS.',
     link: 'https://journal-app-j.netlify.app/'
+},
+{
+    id: 3,
+    title: 'Gift List App',
+    img: 'https://res.cloudinary.com/dob4dwvni/image/upload/v1642653471/Capture_jpbvye.png',
+    description: 'An app to create a gift list. ',
+    link: 'https://gift-list-j.netlify.app/'
 }]
